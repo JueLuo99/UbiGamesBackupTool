1473f341af65b3d18b2658f2cf99e6c0266beac1
绝洛 <jue_luo@yeah.net>
2018-01-20 20:10:25 +0800

    文件操作部分完成

--- UbiGamesBackupTool/Form1.Designer.cs
+++ UbiGamesBackupTool/Form1.Designer.cs
@@ -29,30 +29,31 @@
         private void InitializeComponent()
         {
             this.buttonBackup = new System.Windows.Forms.Button();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialogBackupFrom = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxBackupFrom = new System.Windows.Forms.TextBox();
             this.buttonBackupFrom = new System.Windows.Forms.Button();
-            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialogBackupTo = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxBackupTo = new System.Windows.Forms.TextBox();
             this.buttonBackupTo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonBackup
             // 
-            this.buttonBackup.Location = new System.Drawing.Point(79, 154);
+            this.buttonBackup.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonBackup.Location = new System.Drawing.Point(37, 153);
             this.buttonBackup.Name = "buttonBackup";
             this.buttonBackup.Size = new System.Drawing.Size(99, 52);
             this.buttonBackup.TabIndex = 0;
-            this.buttonBackup.Text = "Backup";
+            this.buttonBackup.Text = "开始备份";
             this.buttonBackup.UseVisualStyleBackColor = true;
             this.buttonBackup.Click += new System.EventHandler(this.buttonBackup_Click);
             // 
-            // textBox1
+            // textBoxBackupFrom
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(305, 21);
-            this.textBox1.TabIndex = 1;
+            this.textBoxBackupFrom.Location = new System.Drawing.Point(12, 26);
+            this.textBoxBackupFrom.Name = "textBoxBackupFrom";
+            this.textBoxBackupFrom.Size = new System.Drawing.Size(305, 21);
+            this.textBoxBackupFrom.TabIndex = 1;
             // 
             // buttonBackupFrom
             // 
@@ -64,12 +65,12 @@
             this.buttonBackupFrom.UseVisualStyleBackColor = true;
             this.buttonBackupFrom.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // textBoxBackupTo
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 88);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(305, 21);
-            this.textBox2.TabIndex = 3;
+            this.textBoxBackupTo.Location = new System.Drawing.Point(12, 88);
+            this.textBoxBackupTo.Name = "textBoxBackupTo";
+            this.textBoxBackupTo.Size = new System.Drawing.Size(305, 21);
+            this.textBoxBackupTo.TabIndex = 3;
             // 
             // buttonBackupTo
             // 
@@ -87,9 +88,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(506, 227);
             this.Controls.Add(this.buttonBackupTo);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxBackupTo);
             this.Controls.Add(this.buttonBackupFrom);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxBackupFrom);
             this.Controls.Add(this.buttonBackup);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -101,11 +102,11 @@
         #endregion
 
         private System.Windows.Forms.Button buttonBackup;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogBackupFrom;
+        private System.Windows.Forms.TextBox textBoxBackupFrom;
         private System.Windows.Forms.Button buttonBackupFrom;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogBackupTo;
+        private System.Windows.Forms.TextBox textBoxBackupTo;
         private System.Windows.Forms.Button buttonBackupTo;
     }
 }
--- UbiGamesBackupTool/Form1.cs
+++ UbiGamesBackupTool/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,22 +18,81 @@ namespace UbiGamesBackupTool
             InitializeComponent();
         }
 
+
+
+
+        private void CopyDirectory(string srcdir, string desdir)
+        {
+            string folderName = srcdir.Substring(srcdir.LastIndexOf("\\") + 1);
+
+            string desfolderdir = desdir + "\\" + folderName;
+
+            if (desdir.LastIndexOf("\\") == (desdir.Length - 1))
+            {
+                desfolderdir = desdir + folderName;
+            }
+            string[] filenames = Directory.GetFileSystemEntries(srcdir);
+
+            foreach (string file in filenames)// 遍历所有的文件和目录
+            {
+                if (Directory.Exists(file))// 先当作目录处理如果存在这个目录就递归Copy该目录下面的文件
+                {
+
+                    string currentdir = desfolderdir + "\\" + file.Substring(file.LastIndexOf("\\") + 1);
+                    if (!Directory.Exists(currentdir))
+                    {
+                        Directory.CreateDirectory(currentdir);
+                    }
+
+                    CopyDirectory(file, desfolderdir);
+                }
+
+                else // 否则直接copy文件
+                {
+                    string srcfileName = file.Substring(file.LastIndexOf("\\") + 1);
+
+                    srcfileName = desfolderdir + "\\" + srcfileName;
+
+
+                    if (!Directory.Exists(desfolderdir))
+                    {
+                        Directory.CreateDirectory(desfolderdir);
+                    }
+
+
+                    File.Copy(file, srcfileName,true);
+                }
+            } 
+        }
+
+
+
         private void buttonBackup_Click(object sender, EventArgs e)
         {
-
+            CopyDirectory(textBoxBackupFrom.Text,textBoxBackupTo.Text);
+            MessageBox.Show("备份完成！");
         }
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            if (folderBrowserDialogBackupFrom.ShowDialog() == DialogResult.OK)
             {
-                this.textBox1.Text = folderBrowserDialog1.SelectedPath;
+                textBoxBackupFrom.Text = folderBrowserDialogBackupFrom.SelectedPath;
             }
         }
 
+
+
+
         private void buttonBackupTo_Click(object sender, EventArgs e)
         {
-
+            if (folderBrowserDialogBackupTo.ShowDialog() == DialogResult.OK)
+            {
+                textBoxBackupTo.Text = folderBrowserDialogBackupTo.SelectedPath;
+            }
         }
     }
 }
